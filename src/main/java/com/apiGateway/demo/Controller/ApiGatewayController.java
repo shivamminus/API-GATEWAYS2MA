8bca6edfbbea3e754d55fcece84787a14c0eb359
Dubey <899702@cognizant.com>
2021-05-28 21:22:38 +0530

    changes in YML for swagger

--- src/main/java/com/apiGateway/demo/Controller/ApiGatewayController.java
+++ src/main/java/com/apiGateway/demo/Controller/ApiGatewayController.java
@@ -6,18 +6,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiGatewayController {
 
+	/*
+	 * Fallback method for Authorization Service
+	 */
 	@GetMapping("/authservicenotresponding")
 	public String showCause() {
 		return "Auth Service is taking too long to respond";
 	}
 
+	/*
+	 * Fallback method for Component Service
+	 */
 	@GetMapping("/componentServiceNotResponding")
 	public String showCause1() {
-		return "Component Service is taking too long to respond";	
+		return "Component Service is taking too long to respond";
 	}
 
-	@GetMapping("/bookingServiceNotResponding")
+	/*
+	 * Fallback method for Packaging Service
+	 */
+	@GetMapping("/PackagingServiceNotResponding")
 	public String showCause2() {
+		return "Packaging Service is taking too long to respond.. Please Try after sometime";
+	}
+
+	/*
+	 * Fallback method for Payment Service
+	 */
+	@GetMapping("/PaymentServiceNotResponding")
+	public String showCause3() {
 		return "Booking Service is taking too long to respond.. Please Try after sometime";
 	}
 
