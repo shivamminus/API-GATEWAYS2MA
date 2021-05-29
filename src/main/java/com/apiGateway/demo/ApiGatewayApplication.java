193f08c1cfc5c25669cef3bc0ad159569fc72c29
shivamminus <dubeyshivam044@gmail.com>
2021-05-29 19:48:46 +0530

    Added Logger and Swagger

--- src/main/java/com/apiGateway/demo/ApiGatewayApplication.java
+++ src/main/java/com/apiGateway/demo/ApiGatewayApplication.java
@@ -1,5 +1,7 @@
 package com.apiGateway.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,8 +11,11 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableEurekaClient
 @EnableHystrix
 public class ApiGatewayApplication {
+	private static Logger logger = LoggerFactory.getLogger(ApiGatewayApplication.class);
 
 	public static void main(String[] args) {
+        System.setProperty("server.connection-timeout","120000");
+        logger.info("API GATEWAY APPLICATION STARTING");
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
